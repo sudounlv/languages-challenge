a15b054109c72ee6884e9e6b6c22fb31eaf8a14f
kaukeb <kaukeb@gmail.com>
2013-10-24 16:26:43 -0700

    Update haskell shell

--- shell-code/haskell/haskell-shell.hs
+++ shell-code/haskell/haskell-shell.hs
@@ -1,1 +1,18 @@
 import qualified Data.Set as Set
+
+-- Find the length of the longest substring with no repeating characters
+lengthOfLongestNRCSubstring :: [Char] -> Int
+lengthOfLongestNRCSubstring str = 0
+
+-- print a test result
+runExample str = do
+  putStrLn $ str ++ ": " ++ result
+    where result = show . lengthOfLongestNRCSubstring $ str
+
+-- main program
+main = do
+  runExample "aaaaaaaaaa"
+  runExample "abc"
+  runExample "hello"
+  runExample "welcomefriend"
+  runExample "abcdefaghi"
